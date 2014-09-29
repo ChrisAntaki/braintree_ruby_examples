8d76df2b8753eecb065583606a68c88cdfb2c226
Chris Antaki <ChrisAntaki@gmail.com>
2014-09-29 15:22:09 -0700

    Updated `braintree_customer_id` to type `string`
    Strings can be up to 255 characters long. Should this be enough for an ID?

--- rails3_tr_devise/db/schema.rb
+++ rails3_tr_devise/db/schema.rb
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(:version => 20110725141224) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                                            :null => false
     t.datetime "updated_at",                                            :null => false
-    t.text     "braintree_customer_id"
+    t.string   "braintree_customer_id"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
